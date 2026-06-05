5ebcf86b2fcae2201f442e8d39ff9f693726b395
David R Git <perfectbrake1@gmail.com>
2026-06-05 11:54:50 -0500

    DR - feat: agregar producto Transporte Turístico Autorizado a la lista de facturación

--- lab/01-facturacion/01-facturacion.js
+++ lab/01-facturacion/01-facturacion.js
@@ -4,7 +4,8 @@ const baseDatosProductos = [
     { id: "P002", descripcion: "Almuerzo Turístico / Gastronomía Autorizada", precio: 12.50, aplicaIvaReducido: true },
     { id: "P003", descripcion: "Tour Operado Centro Histórico Quito", precio: 25.00, aplicaIvaReducido: true },
     { id: "P004", descripcion: "Bebida Energizante Importada", precio: 3.50, aplicaIvaReducido: false },
-    { id: "P005", descripcion: "Souvenir / Artesanía Local", precio: 15.00, aplicaIvaReducido: false }
+    { id: "P005", descripcion: "Souvenir / Artesanía Local", precio: 15.00, aplicaIvaReducido: false },
+    { id: "P006", descripcion: "Transporte Turístico Autorizado", precio: 18.00, aplicaIvaReducido: true }
 ];
 
 // Matriz de Feriados Nacionales en formato ISO
